6bbf1bbd2794dd176b493ef7fa8918fa99a44dc6
ableigdev <igor.yarovoy.98@ukr.net>
2018-03-27 21:05:42 +0300

    Created CMake-file for unit-test project

---
+++ tests/DOMParserTests.cpp
@@ -0,0 +1,21 @@
+﻿#include "gtest/gtest.h"
+#include "DOMParser.h"
+#include "Teg.h"
+
+TEST(ClassTeg, EmptyClass)
+{
+	Teg someTeg;
+
+	std::string name = someTeg.getTegName();
+	std::string parent = someTeg.getParent();
+	std::vector<std::string> children = someTeg.getChildren();
+	std::vector<std::pair<std::string, std::string>> tegProperties = someTeg.getTegProperties();
+	std::string content = someTeg.getContent();
+
+	EXPECT_TRUE(name.empty());
+	EXPECT_TRUE(parent.empty());
+	EXPECT_TRUE(children.empty());
+	EXPECT_TRUE(tegProperties.empty());
+	EXPECT_TRUE(content.empty());
+}
+
