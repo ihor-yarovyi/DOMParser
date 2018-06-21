1f60e55db9909f502ed7e00721af05452effc30a
ableigdev <igor.yarovoy.98@ukr.net>
2018-06-22 00:35:09 +0300

    Added tests for the AttributeValueParser

--- tests/ParserTest/ParserTest.cpp
+++ tests/ParserTest/ParserTest.cpp
@@ -1,7 +1,8 @@
 #include "gtest/gtest.h"
 #include "BaseParser.h"
 #include "TegNameParser.h"
-#include <AttributeParser.h>
+#include "AttributeParser.h"
+#include "AttributeValueParser.h"
 
 #include <memory>
 
@@ -72,6 +73,38 @@ TEST(ParserAttribute, InvalidCase)
     std::vector<std::string> result = ptr->parse();
 
     EXPECT_TRUE(result.empty());
+}
+
+TEST(ParserAttributeValue, ValidCase)
+{
+    std::string inputData("<body width=4><div class=\"name\" with = '2'>Content</div><i id='someid'>str</i><b size = \"5\">str2</b></body>");
+    std::shared_ptr<BaseParser> ptr = std::make_shared<AttibuteValueParser>(inputData);
+    std::vector<std::string> expectResult;
+
+    expectResult.emplace_back("4");
+    expectResult.emplace_back("name");
+    expectResult.emplace_back("2");
+    expectResult.emplace_back("someid");
+    expectResult.emplace_back("5");
+
+    std::vector<std::string> result = ptr->parse();
+
+    EXPECT_EQ(expectResult.size(), result.size());
+    EXPECT_EQ(expectResult[0], result[0]);
+    EXPECT_EQ(expectResult[1], result[1]);
+    EXPECT_EQ(expectResult[2], result[2]);
+    EXPECT_EQ(expectResult[3], result[3]);
+}
+
+TEST(ParseAttributeValue, InvalidCase)
+{
+    std::string inputData("<body width=\"4'><div class=name\" with 2></div><i id='someid\">str</i><b size ~ 5>str2</b></body>");
+    std::shared_ptr<BaseParser> ptr = std::make_shared<AttibuteValueParser>(inputData);
+
+    std::vector<std::string> result = ptr->parse();
+
+    EXPECT_EQ(result[0], "");
+    EXPECT_EQ(result.size(), 1);
 }
 
 int main(int argc, char** argv)
