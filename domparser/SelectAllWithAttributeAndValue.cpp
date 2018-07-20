f70ee851c33775e36d2d034c27631fd112b82277
ableigdev <igor.yarovoy.98@ukr.net>
2018-07-20 15:00:47 +0300

    Add the rule for select tags with this attribute and his value

---
+++ domparser/SelectAllWithAttributeAndValue.cpp
@@ -0,0 +1,28 @@
+#include "SelectAllWithAttributeAndValue.h"
+#include <iostream>
+
+SelectAllWithAttributeAndValue::SelectAllWithAttributeAndValue(const std::cmatch& cm)
+: m_Match(cm.begin(), cm.end())
+{
+}
+
+bool SelectAllWithAttributeAndValue::checkRules(Tag* tag) const
+{
+    if (tag != nullptr)
+    {
+        auto attribute = tag->getAttributeTag();
+        auto attributeValue = tag->getAttributeValueTag();
+
+        if (attribute.size() == attributeValue.size())
+        {
+            for (size_t i = 0; i < attribute.size(); ++i)
+            {
+                if (attribute[i] == m_Match[2] && attributeValue[i] == m_Match[3])
+                {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
---
+++ domparser/SelectAllWithAttributeAndValue.h
@@ -0,0 +1,17 @@
+#ifndef DOMPARSER_SELECTALLWITHATTRIBUTEANDVALUE_H
+#define DOMPARSER_SELECTALLWITHATTRIBUTEANDVALUE_H
+
+#include "CheckRulesFactory.h"
+
+class SelectAllWithAttributeAndValue : public CheckRulesFactory
+{
+public:
+    SelectAllWithAttributeAndValue(const std::cmatch&);
+    virtual bool checkRules(Tag*) const;
+
+private:
+    std::vector<std::string> m_Match;
+};
+
+
+#endif //DOMPARSER_SELECTALLWITHATTRIBUTEANDVALUE_H
