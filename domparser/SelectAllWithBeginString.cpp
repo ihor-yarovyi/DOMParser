0ef7bc8a4b83fb92306ccc2e688e571770d045bf
ableigdev <igor.yarovoy.98@ukr.net>
2018-07-21 11:19:12 +0300

    Add the rule for select tags with an attribute in begin a string

---
+++ domparser/SelectAllWithBeginString.cpp
@@ -0,0 +1,29 @@
+#include "SelectAllWithBeginString.h"
+
+SelectAllWithBeginString::SelectAllWithBeginString(const std::cmatch& cm)
+: m_Match(cm.begin(), cm.end())
+{
+
+}
+
+bool SelectAllWithBeginString::checkRules(Tag* tag) const
+{
+    if (tag != nullptr)
+    {
+        auto attribute = tag->getAttributeTag();
+        auto attributeValue = tag->getAttributeValueTag();
+
+        if (attribute.size() == attributeValue.size())
+        {
+            std::cmatch cm;
+            for (size_t i = 0; i < attribute.size(); ++i)
+            {
+                if (attribute[i] == m_Match[2] && std::regex_search(attributeValue[i].data(), cm, std::regex("^(" + m_Match[3] + ")")))
+                {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
---
+++ domparser/SelectAllWithBeginString.h
@@ -0,0 +1,16 @@
+#ifndef DOMPARSER_SELECTALLWITHBEGINSTRING_H
+#define DOMPARSER_SELECTALLWITHBEGINSTRING_H
+
+#include "CheckRulesFactory.h"
+
+class SelectAllWithBeginString : public CheckRulesFactory
+{
+public:
+    SelectAllWithBeginString(const std::cmatch&);
+    virtual bool checkRules(Tag*) const;
+
+private:
+    std::vector<std::string> m_Match;
+};
+
+#endif //DOMPARSER_SELECTALLWITHBEGINSTRING_H
